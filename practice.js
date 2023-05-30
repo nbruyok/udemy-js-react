d80c59fc7f525619f51987b868e4a628e35a9412
Nastya Bruyok <nastyabruyok@gmail.com>
2023-05-30 21:29:43 +0300

    feat: implement practice task-2

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieBD = {
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
@@ -8,12 +8,27 @@ const personalMovieBD = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его ?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его ?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', '');
+  const b = prompt('На сколько оцените его?', '');
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
-console.log(personalMovieBD);
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
